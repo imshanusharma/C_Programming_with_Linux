a09b0bd8f69f51ccc646acd28abcb9f2ca9f415a
Ujjwal Sharma <shanusharma588@gmail.com>
2021-07-27 19:45:39 +0530

    Update bubble_sort_analysis.c

--- Runtime_analysis_of_Bubble_Sort_Algorithm/bubble_sort_analysis.c
+++ Runtime_analysis_of_Bubble_Sort_Algorithm/bubble_sort_analysis.c
@@ -55,7 +55,7 @@ int main() {
         a[i] = rand();
     }
 	
-    /*Uncomment below part to calculate runtime complexity for the Descending order, to calculate for ascending order replace '<' with '>' in line no. 63*/
+    /*Uncomment below part to calculate runtime complexity for the Descending order, to calculate for ascending order replace '<' with '>' in line no. 65*/
 
    /* int temp1;
     for(int i=0;i<n;i++) {
