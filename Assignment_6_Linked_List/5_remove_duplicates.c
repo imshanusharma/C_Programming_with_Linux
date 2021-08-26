84904652b5b8ba3c434e0d932e5ce7806d30dda7
Ujjwal <shanusharma588@gmail.com>
2021-08-26 11:12:13 +0530

    Added delete duplicates from LL

---
+++ Assignment_6_Linked_List/5_remove_duplicates.c
@@ -0,0 +1,44 @@
+/* Program to remove duplicate elements from a linked list
+ *
+ * LeetCode Q-82
+ *
+ * @Ujjwal, 1910991390, 26/08/2021
+ * Assignment 6 (Linked List)
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+struct ListNode deleteDuplicates(struct ListNode *head) {
+
+    //edge cases
+
+    if(head == NULL || head -> next == NULL)
+    {
+	return head;
+    }
+
+    //if distinct elements found
+
+    if(head -> val != head -> next -> val)
+    {
+	head -> next = deleteDuplicates(head->next);
+	return head;
+    }
+
+    //removing duplicates
+
+    struct ListNode *temp = head;
+
+    while(temp != NULL && temp -> val == head -> val)
+    {
+	temp = temp -> next;
+    }
+    
+    return deleteDuplicates(temp);
+}
